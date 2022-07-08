c0c104a4ee41cdee90da9a620752207f1144ac58
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-07-08 20:10:16 +0800

    Update PreInstall.sh

--- PreInstall.sh
+++ PreInstall.sh
@@ -20,6 +20,12 @@ echo "正在配置IPV4转发. . ."
 
 echo 'net.ipv4.ip_forward = 1' > /etc/sysctl.conf
 
+echo 'fs.file-max = 51200' >> /etc/sysctl.conf
+
+echo 'net.core.default_qdisc = fq' >> /etc/sysctl.conf
+
+echo 'net.ipv4.tcp_congestion_control = bbr' >> /etc/sysctl.conf
+
 sysctl -p
 
 echo "优化完成!"
