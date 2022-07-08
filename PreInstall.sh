b0e484c1b99a65eb9a4c3b1a46475644a6b09eb1
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-07-08 19:32:21 +0800

    Update PreInstall.sh

--- PreInstall.sh
+++ PreInstall.sh
@@ -8,13 +8,17 @@ echo '* soft nofile 65536' > /etc/security/limits.conf
 
 echo '* hard nofile 65536' >> /etc/security/limits.conf
 
-echo 'root soft nofile 65536 >> /etc/security/limits.conf
+echo 'root soft nofile 65536' >> /etc/security/limits.conf
 
-echo 'root hard nofile 65536 >> /etc/security/limits.conf
+echo 'root hard nofile 65536' >> /etc/security/limits.conf
 
-echo '* soft memlock unlimited >> /etc/security/limits.conf
+echo '* soft memlock unlimited' >> /etc/security/limits.conf
 
-echo '* hard memlock unlimited >> /etc/security/limits.conf
+echo '* hard memlock unlimited' >> /etc/security/limits.conf
+
+echo "正在配置IPV4转发. . ."
+
+echo 'net.ipv4.ip_forward = 1' > /etc/sysctl.conf
 
 echo "优化完成!"
 
