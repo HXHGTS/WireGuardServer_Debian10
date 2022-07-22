29e28187644e670bd56e872d6009c5df8a9b0dd8
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-07-22 17:50:33 +0800

    Update PreInstall.sh

--- PreInstall.sh
+++ PreInstall.sh
@@ -2,31 +2,11 @@
 
 apt install -y build-essential net-tools iproute2 wget
 
-echo "正在优化大文件读写性能. . ."
-
-echo '* soft nofile 65536' > /etc/security/limits.conf
-
-echo '* hard nofile 65536' >> /etc/security/limits.conf
-
-echo 'root soft nofile 65536' >> /etc/security/limits.conf
-
-echo 'root hard nofile 65536' >> /etc/security/limits.conf
-
-echo '* soft memlock unlimited' >> /etc/security/limits.conf
-
-echo '* hard memlock unlimited' >> /etc/security/limits.conf
-
 echo "正在配置IPV4转发. . ."
 
 echo 'net.ipv4.ip_forward = 1' > /etc/sysctl.conf
 
-#echo 'net.ipv6.conf.all.forwarding = 1' >> /etc/sysctl.conf
-
-echo 'fs.file-max = 51200' >> /etc/sysctl.conf
-
-echo 'net.core.default_qdisc = fq' >> /etc/sysctl.conf
-
-echo 'net.ipv4.tcp_congestion_control = bbr' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding = 1' >> /etc/sysctl.conf
 
 sysctl -p
 
