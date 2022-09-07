723c634aebc384c21c19be6ebbe64348f6a7dadb
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-09-07 08:38:14 +0800

    Update PreInstall.sh

--- PreInstall.sh
+++ PreInstall.sh
@@ -8,6 +8,10 @@ echo 'net.ipv4.ip_forward = 1' > /etc/sysctl.conf
 
 echo 'net.ipv6.conf.all.forwarding = 1' >> /etc/sysctl.conf
 
+echo 'net.core.default_qdisc = fq' >> /etc/sysctl.conf
+
+echo 'net.ipv4.tcp_congestion_control = bbr' >> /etc/sysctl.conf
+
 sysctl -p
 
 echo "优化完成!"
