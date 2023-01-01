8c49a18ebeacd6357c15490df221a7e63a5cb005
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-01-01 13:14:07 +0800

    Create PreInstall6.sh

---
+++ PreInstall6.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+apt install -y build-essential net-tools iproute2 wget
+
+echo "正在配置IPV4/IPV6转发. . ."
+
+echo 'net.ipv4.ip_forward = 1' > /etc/sysctl.conf
+
+echo 'net.ipv6.conf.all.forwarding = 1' >> /etc/sysctl.conf
+
+echo "正在开启BBR. . ."
+
+echo 'net.core.default_qdisc = fq' >> /etc/sysctl.conf
+
+echo 'net.ipv4.tcp_congestion_control = bbr' >> /etc/sysctl.conf
+
+sysctl -p
+
+echo "优化完成!"
+
+exit 0
